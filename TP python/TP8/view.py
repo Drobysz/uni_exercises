b51cbf702028d54ed4dcfaba5dcf43f5f0808d13
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-19 01:49:54 +0100

    * Create library with view methodes
    - view_case: it views one case
    - view_cases: it views list of cases

---
+++ TP python/TP8/view.py
@@ -0,0 +1,16 @@
+from fltk import *
+
+def view_case(cell, color, size):
+		b1, b2 = cell[0] * size, cell[1] * size
+		e1, e2 = b1 + size, b2 + size,
+
+		rectangle(
+			b1, b2,
+			e1, e2,
+			color,
+			color
+		)
+
+def view_cases(lst, color, size):
+	for cell in lst:
+		view_case(cell, color, size)
