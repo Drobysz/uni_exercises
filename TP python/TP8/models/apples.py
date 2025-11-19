9b966d745a9a3c72b9d1ca2e2995359f6e17ff7a
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-19 09:36:54 +0100

    * Make randrange in apple init equal to num of cases

--- TP python/TP8/models/apples.py
+++ TP python/TP8/models/apples.py
@@ -3,10 +3,10 @@ from .model_classes import Coord
 from view import view_cases
 
 class Apples:
-	def __init__(self, nb_apples):
+	def __init__(self, nb_apples, nb_cases):
 		self.nb_apples: int = nb_apples
 		self.apples = [
-      		(randrange(40), randrange(40)) for _ in range(self.nb_apples)
+      		(randrange(nb_cases), randrange(nb_cases)) for _ in range(self.nb_apples)
         ]
 
 	def render(self, case_size): view_cases(self.apples, 'red', case_size)
