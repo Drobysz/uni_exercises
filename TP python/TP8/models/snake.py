f3937d92687d4799853d4ccc7d6ae69617b53596
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-19 02:00:58 +0100

    * Create model Snake
    Props:
    - size: initial lenght of snake
    - dir: current snake directory
    - body: list of snake cell coordinates
    
    Methodes:
    - render: rendering of snake
    - isEncountered: checking if snake encountered itself of map borders
    - current_coordinates: coordinates of snake head
    - body_positive: adding of 1 cell to the end of the list
    - sheft_snake: moving of snake per 1 cell to current directory
    - change_dir: changing of current directory

---
+++ TP python/TP8/models/snake.py
@@ -0,0 +1,73 @@
+from .model_classes import *
+from view import view_case
+
+def define_cell_coordinates(l_c, dir):
+	match dir:
+		case "Down":
+			return (l_c[0], l_c[1] + 1)
+		case "Up":
+			return (l_c[0], l_c[1] - 1)
+		case "Left":
+			return (l_c[0] + 1, l_c[1])
+		case "Right":
+			return (l_c[0] - 1, l_c[1])
+
+def define_next_coordinates(c, dir):
+	match dir:
+		case "Down":
+			return (c[0], c[1] - 1)
+		case "Up":
+			return (c[0], c[1] + 1)
+		case "Left":
+			return (c[0] - 1, c[1])
+		case "Right":
+			return (c[0] + 1, c[1])
+
+dirs = {'Up': 'Down', 'Down': 'Up', 'Left': 'Left', 'Right': 'Right'}
+opposite = {'Up': 'Down', 'Down': 'Up', 'Left': 'Right', 'Right': 'Left'}
+
+class Snake:
+	def __init__(s, size):
+		s.size: int = size
+		s.dir: Direction = 'Right'
+		s.body: Body = [(i + 5, 20) for i in range(s.size)]
+
+	@property
+	def ln(s): return len(s.body)
+
+	def render(s, case_size):
+		for i in range(s.ln):
+			view_case(s.body[i], 'green' if i < s.ln - 1 else 'blue', case_size)
+ 
+	def isEncountered(s, width, case_size):
+		head = s.body[-1]
+		rest_of_body = s.body[:s.ln-1]
+		
+		if head in rest_of_body:
+			return True
+		x_px = head[0] * case_size
+		y_px = head[1] * case_size
+
+		if x_px < 0 or x_px >= width or y_px < 0 or y_px >= width:
+			return True
+		return False
+
+	def current_coordinates(s):
+		return s.body[-1]
+
+	def change_dir(s, key):
+		if key in ('Up', 'Down', 'Left', 'Right'):
+			dir = dirs[key] 
+			if opposite[dir] != s.dir:
+				s.dir = dirs[key]    
+	def body_positive(s):
+		l_c: Coord = s.body[0]
+		n_c = define_cell_coordinates(l_c, s.dir)
+
+		s.body = [n_c] + s.body[:]
+
+	def shift_snake(s):
+		for id in range(s.ln - 1):
+			next_coord = s.body[id + 1]
+			s.body[id] = next_coord
+		s.body[s.ln - 1] = define_next_coordinates(s.body[s.ln - 1], s.dir)
