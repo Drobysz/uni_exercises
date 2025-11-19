ff353e2afc82d78eec17501d15ff1da8aa9a5ab8
Drobysz <alexanderdrobyschevski@gmail.com>
2025-11-19 01:52:00 +0100

    * Create model Window
    Description: it's responsible for:
    	1) rendering of all elements on tne map
    	2) displaying of headers
    	3) window and cases configuration

---
+++ TP python/TP8/window.py
@@ -0,0 +1,39 @@
+from fltk import *
+from view import *
+from models.apples import *
+from models.snake import *
+
+class Window: 
+	def __init__(self, case_size, width, nb_cases):	
+		self.case_size: int = case_size
+		self.width: int = width
+		self.nb_cases: int = nb_cases
+
+		assert (self.width % self.nb_cases == 0)
+		cree_fenetre(self.width, self.width)
+		texte(
+			self.width // 2, self.width // 2,
+			"Tapez sur une touche !", "red", "center"
+		)
+
+		attend_ev()
+ 
+	def render(self, snake, apples):
+		efface_tout()
+		snake.render(self.case_size)
+		apples.render(self.case_size)
+		mise_a_jour()
+ 
+	def win(self):
+		efface_tout()
+		texte(
+			self.width // 2, self.width // 2,
+			"Gagné! pour continuer clicker ou Esc pour finir", "green", "center"
+		)
+
+	def loss(self):
+		efface_tout()
+		texte(
+			self.width // 2, self.width // 2,
+			"Perdu! pour continuer clicker ou Esc pour finir", "red", "center"
+		)
